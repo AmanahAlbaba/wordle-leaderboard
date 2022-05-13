81e15d18a9027ad9fc07ce01b1f0f45bdefabacf
Amanah Albaba <83415396+AmanahAlbaba@users.noreply.github.com>
2022-05-13 18:37:56 -0400

    fixed a bug and added messages depending on score

--- cogs/setScore.py
+++ cogs/setScore.py
@@ -16,6 +16,22 @@ class setScore(commands.Cog):
             for i in range (len(message.content)):
                 if(message.content[i]=='/'):
                     score = message.content[i-1]
+                    if (score == 'X'):
+                        score = 7
+                    if (score == '1'):
+                        await message.channel.send("You definitely cheated :neutral_face:")
+                    elif (score == '2'):
+                        await message.channel.send("A bit sus :face_with_raised_eyebrow:")
+                    elif (score == '3'):
+                        await message.channel.send("Bruh :unamused:")
+                    elif (score == '4'):
+                        await message.channel.send("You did the bare minimum :yawning_face:")
+                    elif (score == '5'):
+                        await message.channel.send("At least you didn't get it in 6 :poop:")
+                    elif (score== '6'):
+                        await message.channel.send("L :clown:")
+                    elif (score == 7):
+                        await message.channel.send("RIP you suck :skull:")
                     break
             for file in os.listdir():
                 if(file == str(id) + ".csv"):
