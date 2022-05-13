626198dfd012d6ba5664ecb265f11f3157aa9546
Amanah Albaba <83415396+AmanahAlbaba@users.noreply.github.com>
2022-05-13 08:28:12 -0400

    added ranking to myStats

--- cogs/getStats.py
+++ cogs/getStats.py
@@ -1,20 +1,53 @@
 import pandas as pd
 from nextcord.ext import commands
+import os
+
 class getStats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.command()
     async def myStats (self, ctx):
-        totalScore = 0
-        days = 0
         id = ctx.message.author.id
         try:
             df = pd.read_csv(str(id) + '.csv')
             totalScore = df['Score'].sum()
             days = df['Score'].count()
             avg = totalScore/days
-            await ctx.send(f"```Stats for {ctx.author.name} \nTotal Number of wordles played: " + str(days) + "\nTotal Number of Guesses: " + str(totalScore) + "\nAverage Number of Guesses: " + str(avg) + "```")
+            ranking = self.rank(id)
+            await ctx.send(f"```Stats for {ctx.author.name} \nTotal Number of wordles played: " + str(days) + "\nTotal Number of Guesses: " + str(totalScore) + "\nAverage Number of Guesses: " + str(avg)  + "\nRanking: " + str(ranking) + "```")
         except OSError:
             await ctx.send("You do not have any plays yet")
+
+    def calcAvg (self):
+        ids = []
+        avg = []
+        for filename in os.listdir():
+            if (filename.endswith(".csv")):
+                df = pd.read_csv(filename)
+                avg.append(df['Score'].sum()/df['Score'].count())
+                ids.append(filename.split(".")[0])
+        return ids, avg
+
+    def rank(self, id):
+        ids, avg = self.calcAvg()
+        for i in range(len(avg)):
+            swap = False
+            for j in range(len(avg) - i - 1):
+                if (avg[j] > avg[j+1]):
+                    temp = avg[j]
+                    avg[j] = avg[j+1]
+                    avg[j+1] = temp
+                    temp = ids[j]
+                    ids[j] = ids[j+1]
+                    ids[j+1] = temp
+                    swap = True
+            if (swap != True):
+                break;
+        for i in range (len(avg)):
+            if (str(ids[i]) == str(id)):
+                break
+
+        return (i+1)
+
 def setup(bot):
     bot.add_cog(getStats(bot))
